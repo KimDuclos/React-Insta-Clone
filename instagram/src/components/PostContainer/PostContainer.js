1173217bd0ebde65d75d107ec12f187b887ce588
Kim Duclos <kimduclos@gmail.com>
2019-02-18 15:14:27 -0700

    basic postContainer parts built

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PostContainer.css';
+
+const PostContainer = props => {
+return (
+    <div className="postBox">
+        <div>POST ARRAY WILL GO HERE</div>
+    </div>
+)};
+    
+    
+
+export default PostContainer;
