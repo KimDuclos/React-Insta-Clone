4192a093fc2b6602140787fdbe28ecef33db4765
Kim Duclos <kimduclos@gmail.com>
2019-02-18 15:31:47 -0700

    basic css done for postContainer, searchBar and CommentSection

---
+++ instagram/src/components/CommentsSection/CommentsSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CommentSection.css';
+
+const CommentSection = props => {
+return (
+    <div className="commentInput">
+        <div className="comment">COMMENT INPUT GOES HERE</div>
+        <div className="userName">USERNAME GOES HERE</div>
+    </div>
+)};
+
+export default CommentSection;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '';
 import './SearchBar.css';
 
 const SearchBar = () => {
