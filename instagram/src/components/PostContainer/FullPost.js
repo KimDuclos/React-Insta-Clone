bd11603fdc03a318a6a9145ec464717d33756dc5
Kim Duclos <kimduclos@gmail.com>
2019-02-20 14:38:22 -0700

    like/comment icons and like increment built

--- instagram/src/components/PostContainer/FullPost.js
+++ instagram/src/components/PostContainer/FullPost.js
@@ -4,45 +4,49 @@ import Header from './Header';
 import './PostContainer.css';
 import Likes from './Likes';
 
+//set state of likes + add property
 class FullPost extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {likes: props.post.likes}
+    this.state = {userLike: props.post.userLike}
   };
+
+  incLikes = () => {
+    let likes = this.state.userLike;
+    this.setState({userLike: likes++});
+  };
+
+  render() {
+    return (
+      // border for entire post and comments
+      <div className="border">
+        {/* header component that gets dummy data */}
+        <Header 
+          // username from dummy data
+          username={this.props.post.username}
+          // user thumbnail from dummy data
+          thumbnailUrl={this.props.post.thumbnailUrl}
+        />
+        {/* container for only the image */}
+        <div className="imgContainer">
+          <img
+            alt="posted img"
+            className="image"
+            // dummy data from imgURL
+            src={this.props.post.imageUrl}
+          />
+        </div>
+        {/* compenent that gets comments from dummy data */}
+        <Likes incLikes={this.incLikes} likes={this.state.likes}/>
+        <CommentsSection comments={this.props.post.comments} />
+      </div>
+      );
+  }
 }
 
-// function to increment likes
-incLike = () => {
-  this.setState({likes});
-};
+
 
 //function to create post container not including contents
-const FullPost = props => {
-  return (
-
-    // border for entire post and comments
-    <div className="border">
-      {/* header component that gets dummy data */}
-      <Header 
-        // username from dummy data
-        username={props.post.username}
-        // user thumbnail from dummy data
-        thumbnailUrl={props.post.thumbnailUrl}
-      />
-      {/* container for only the image */}
-      <div className="imgContainer">
-        <img
-          alt="posted img"
-          className="image"
-          // dummy data from imgURL
-          src={props.post.imageUrl}
-        />
-      </div>
-      {/* compenent that gets comments from dummy data */}
-      <Likes incrementLike={this.incLikes} likes={this.state.likes}/>
-      <CommentsSection comments={props.post.comments} />
-    </div>
-    );
-  }
+  
 
 export default FullPost;
